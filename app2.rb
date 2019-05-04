5ccadfa4f5b7720f6e323fec03408f6ba472077c
Alex Nazarov <pokupka91@mail.ru>
2019-05-04 06:01:25 +0300

    first

---
+++ app2.rb
@@ -0,0 +1,17 @@
+#qwe
+
+print "Hi"
+
+x = 10
+
+if x == 10
+	begin
+		print x
+	end
+else
+	begin
+		print x
+	end
+
+
+		
---
+++ barbershop.rb
@@ -0,0 +1,38 @@
+require 'sinatra'
+
+get '/' do
+	erb :index
+end
+
+post '/' do
+	@user_name = params[:user_name]
+	@phone = params[:phone]
+	@date_time = params[:date_time]
+	@master = params[:master]
+	
+	@title = 'Thanks'
+	@message = "Dear #{@user_name}, we'll be waiting for you at #{@date_time} to #{@master}"
+	
+	f = File.open 'users.txt', 'a'
+	f.write "User: #{@user_name}, Phone: #{@phone}, Date and Time: #{@date_time}, Master: #{@master}\n"
+	f.close
+
+	erb :message
+end
+
+get '/admin' do
+
+	@message = ""
+	
+	f = File.open 'users.txt', 'r'
+	while (line = f.gets)
+		@message += line 
+	end
+	f.close
+
+	@title = 'Gast list'
+
+	erb :message
+end
+
+
